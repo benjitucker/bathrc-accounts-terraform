a1cffb5c6e0eee3e3835c7a670e8195393c9a286
Benjamin Tucker <benjamint@bsquare.com>
2023-11-13 21:21:45 +0000

    Remove api-gateway access logs and domain name code

--- api-gateway.tf
+++ api-gateway.tf
@@ -20,15 +20,9 @@ module "api_gateway" {
   }
 
   # Custom domain
-  #domain_name                 = "terraform-aws-modules.modules.tf"
-  #domain_name_certificate_arn = "arn:aws:acm:eu-west-1:052235179155:certificate/2b3a7ed9-05e1-4f9e-952b-27744ba06da6"
-  #domain_name                 = local.domain_name
-  #domain_name_certificate_arn = module.acm.acm_certificate_arn
   create_api_domain_name = false
 
   # Access logs
-  #default_stage_access_log_destination_arn = "arn:aws:logs:eu-west-1:835367859851:log-group:debug-apigateway"
-  #default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
 
   # Routes and integrations
   integrations = {

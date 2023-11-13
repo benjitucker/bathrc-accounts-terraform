e24a4b866d62bbd7be580c14f11240e19f47d40d
Benjamin Tucker <benjamint@bsquare.com>
2023-11-13 20:41:10 +0000

    Remove dead code

--- virtual-network.tf
+++ virtual-network.tf
@@ -9,32 +9,12 @@ locals {
   az_count = min(3, length(data.aws_availability_zones.available.names))
   azs      = slice(data.aws_availability_zones.available.names, 0, local.az_count)
 
-  #vpc_id          = module.virtual-network.vpc_id
-  #route_table_id  = module.virtual-network.route_table_id
-  #virtual_network = module.virtual-network
-  #private_subnet = local.virtual_network.private_subnet[*]
-
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnets
 }
 
 # Fetch AZs in the current region
 data "aws_availability_zones" "available" {}
-
-/*
-module "virtual-network" {
-  source      = "./virtual-network"
-  description = "bathrc-accounts"
-  env_name    = var.env_name
-
-  cidr                 = var.vpc_subnet_cidr
-  public_cidr          = local.public_cidr
-  public_subnet_count  = local.az_count
-  private_cidr         = local.private_cidr
-  private_subnet_count = local.az_count
-  tags                 = local.tags
-}
-*/
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -46,9 +26,7 @@ module "vpc" {
   private_subnets      = [local.private_cidr]
   public_subnets       = [local.public_cidr]
   enable_dns_hostnames = true
-  tags = {
-    "Name" = "bathrc-accounts"
-  }
+  tags                 = local.tags
 }
 
 module "nat" {
@@ -64,7 +42,5 @@ module "nat" {
 
 resource "aws_eip" "nat" {
   network_interface = module.nat.eni_id
-  tags = {
-    "Name" = "bathrc-accounts"
-  }
+  tags              = local.tags
 }

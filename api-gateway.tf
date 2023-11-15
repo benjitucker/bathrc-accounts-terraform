8f34ed660c79c070e22f24eec8fd13c70297fb28
Benjamin Tucker <benjamint@bsquare.com>
2023-11-15 22:35:15 +0000

    Experimental APIGW Rest api integrating S3

--- api-gateway.tf
+++ api-gateway.tf
@@ -55,10 +55,11 @@ module "api_gateway" {
      * APIs have the same origin (API GW) and no need for a custom domain
      * See https://repost.aws/knowledge-center/api-gateway-s3-website-proxy
      */
+    /*
     "GET /ui" = {
-      integration_method = "GET"
-      integration_type   = "AWS"
-      /* See https://docs.aws.amazon.com/apigateway/latest/api/API_PutIntegration.html for the uri details */
+      integration_method     = "GET"
+      integration_type       = "AWS"
+      # See https://docs.aws.amazon.com/apigateway/latest/api/API_PutIntegration.html for the uri details
       integration_uri        = "arn:aws:apigateway:${var.aws_region}:s3:path/${local.bucket_name}/index.html"
       credentials_arn        = aws_iam_role.s3_proxy_role.arn
       payload_format_version = "1.0"
@@ -67,7 +68,7 @@ module "api_gateway" {
       response_parameters = jsonencode([
         {
           status_code = 200
-          mappings = {
+          mappings    = {
             "append:header.timestamp"      = "$response.header.date",
             "append:header.content-length" = "$response.header.content-length",
             "append:header.content-type"   = "$response.header.content-type"
@@ -75,6 +76,7 @@ module "api_gateway" {
         }
       ])
     }
+    */
 
     "$default" = {
       #lambda_arn = "arn:aws:lambda:eu-west-1:052235179155:function:my-default-function"
@@ -95,7 +97,7 @@ module "api_gateway" {
   tags = local.tags
 }
 
-output "api-endpoint" {
+output "apigwv2-api-endpoint" {
   value = module.api_gateway.apigatewayv2_api_api_endpoint
 }
 
@@ -133,4 +135,132 @@ data "aws_iam_policy_document" "s3_proxy_policy" {
 resource "aws_iam_role_policy_attachment" "s3_proxy_role_api_gateway_attachment" {
   role       = aws_iam_role.s3_proxy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_api_gateway_rest_api" "MyS3" {
+  name        = "MyS3"
+  description = "API for S3 Integration"
+}
+
+resource "aws_api_gateway_resource" "Folder" {
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  parent_id   = aws_api_gateway_rest_api.MyS3.root_resource_id
+  path_part   = "{folder}"
+}
+
+resource "aws_api_gateway_resource" "Item" {
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  parent_id   = aws_api_gateway_resource.Folder.id
+  path_part   = "{item}"
+}
+
+resource "aws_api_gateway_method" "GetBuckets" {
+  rest_api_id   = aws_api_gateway_rest_api.MyS3.id
+  resource_id   = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method   = "GET"
+  authorization = "AWS_IAM"
+}
+
+resource "aws_api_gateway_integration" "S3Integration" {
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "GET"
+
+  type = "AWS"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  #  uri         = "arn:aws:apigateway:${var.aws_region}:s3:path//"
+  #  credentials = aws_iam_role.s3_api_gateway_role.arn
+  uri         = "arn:aws:apigateway:${var.aws_region}:s3:path/${local.bucket_name}/index.html"
+  credentials = aws_iam_role.s3_proxy_role.arn
+}
+
+resource "aws_api_gateway_method_response" "Status200" {
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Timestamp"      = true
+    "method.response.header.Content-Length" = true
+    "method.response.header.Content-Type"   = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_method_response" "Status400" {
+  depends_on = [aws_api_gateway_integration.S3Integration]
+
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = "400"
+}
+
+resource "aws_api_gateway_method_response" "Status500" {
+  depends_on = [aws_api_gateway_integration.S3Integration]
+
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = "500"
+}
+
+resource "aws_api_gateway_integration_response" "IntegrationResponse200" {
+  depends_on = [aws_api_gateway_integration.S3Integration]
+
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = aws_api_gateway_method_response.Status200.status_code
+
+  response_parameters = {
+    "method.response.header.Timestamp"      = "integration.response.header.Date"
+    "method.response.header.Content-Length" = "integration.response.header.Content-Length"
+    "method.response.header.Content-Type"   = "integration.response.header.Content-Type"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "IntegrationResponse400" {
+  depends_on = [aws_api_gateway_integration.S3Integration]
+
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = aws_api_gateway_method_response.Status400.status_code
+
+  selection_pattern = "4\\d{2}"
+}
+
+resource "aws_api_gateway_integration_response" "IntegrationResponse500" {
+  depends_on = [aws_api_gateway_integration.S3Integration]
+
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+  resource_id = aws_api_gateway_rest_api.MyS3.root_resource_id
+  http_method = aws_api_gateway_method.GetBuckets.http_method
+  status_code = aws_api_gateway_method_response.Status500.status_code
+
+  selection_pattern = "5\\d{2}"
+}
+
+resource "aws_api_gateway_deployment" "S3APIDeployment" {
+  depends_on  = [aws_api_gateway_integration.S3Integration]
+  rest_api_id = aws_api_gateway_rest_api.MyS3.id
+}
+
+resource "aws_api_gateway_stage" "S3APIStage" {
+  deployment_id = aws_api_gateway_deployment.S3APIDeployment.id
+  rest_api_id   = aws_api_gateway_rest_api.MyS3.id
+  stage_name    = "MyS3"
+}
+
+output "apigw-invoke-url" {
+  value = aws_api_gateway_stage.S3APIStage.invoke_url
 }

1d806e4031a11e9dffe5fa5875ddd5bc8b201c27
Benjamin Tucker <benjamint@bsquare.com>
2023-11-22 18:39:07 +0000

    backend API POST method

--- api-gateway.tf
+++ api-gateway.tf
@@ -150,6 +150,13 @@ resource "aws_api_gateway_method" "backend-lambda-get" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "backend-lambda-get" {
+  rest_api_id   = aws_api_gateway_rest_api.MyS3.id
+  resource_id   = aws_api_gateway_resource.backend-lambda.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "backend-lambda" {
   rest_api_id             = aws_api_gateway_rest_api.MyS3.id
   resource_id             = aws_api_gateway_resource.backend-lambda.id

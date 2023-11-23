27950cf6a56524dfcc89d63b02a8595ca732d7cb
Benjamin Tucker <benjamint@bsquare.com>
2023-11-23 16:43:35 +0000

    Redeploy API gateway every time we run the terraform deployment

--- api-gateway.tf
+++ api-gateway.tf
@@ -237,6 +237,7 @@ resource "aws_api_gateway_deployment" "S3APIDeployment" {
   rest_api_id = aws_api_gateway_rest_api.MyS3.id
 
   triggers = {
+    everytime = timestamp()
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.ui.id,
       aws_api_gateway_resource.Item.id,

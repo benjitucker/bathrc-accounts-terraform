d5546739c3c10732755995a9a246bb8bb070fbcd
Benjamin Tucker <benjamint@bsquare.com>
2023-12-05 09:20:56 +0000

    Fix the callback API parent

--- api-gateway.tf
+++ api-gateway.tf
@@ -31,7 +31,7 @@ locals {
 
 module "callback_api" {
   source                  = "./api"
-  parent_id               = aws_api_gateway_resource.ui.id
+  parent_id               = module.ui_api.resource_id
   path_part               = "callback"
   rest_api_id             = aws_api_gateway_rest_api.MyS3.id
   http_methods            = ["GET"]

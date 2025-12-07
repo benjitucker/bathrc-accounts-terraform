7b04d5621eab40382d4933de61c58c8648f747f4
Benjamin Tucker <benjamin.tucker@kontron.com>
2025-12-07 17:53:05 +0000

    Comment out the CORS config of the lambda url for testing

--- lambda/main.tf
+++ lambda/main.tf
@@ -141,12 +141,14 @@ resource "aws_lambda_function_url" "default" {
   //  qualifier          = "my_alias"
   authorization_type = "NONE"
 
+  /*
   cors {
     allow_credentials = true
-    //    allow_origins     = ["https://eu.jotform.com"]
+    allow_origins     = ["https://eu.jotform.com"]
     allow_methods = ["POST"]
     //    allow_headers     = ["date", "keep-alive"]
     //    expose_headers    = ["keep-alive", "date"]
     max_age = 86400
   }
+   */
 }

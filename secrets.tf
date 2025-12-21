ca3b742e1a1d8012586bc59b4c6e326ba2ec4a3c
Benjamin Tucker <benjamin.tucker@kontron.com>
2025-12-21 15:06:47 +0000

    VPC endpoint/sg for lambda access to SSM

--- secrets.tf
+++ secrets.tf
@@ -5,3 +5,32 @@ resource "aws_ssm_parameter" "jotform-apikey" {
   value       = var.jotform_apikey
   tags        = local.tags
 }
+
+resource "aws_security_group" "ssm_vpc_endpoint_sg" {
+  name        = "ssm-vpc-endpoint-sg"
+  description = "Allow Lambda to access SSM endpoint"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_subnet_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [aws_security_group.ssm_vpc_endpoint_sg.id]
+}

3104d203597807a916e1436fd4a871cfcee03736
Benjamin Tucker <benjamin.tucker@kontron.com>
2026-01-05 13:05:13 +0000

    The nat instance network adapter is ens5

--- nat-instance.tf
+++ nat-instance.tf
@@ -87,7 +87,7 @@ sudo systemctl enable iptables
 sudo systemctl start iptables
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/custom-ip-forwarding.conf
 sudo sysctl -p /etc/sysctl.d/custom-ip-forwarding.conf
-sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo /sbin/iptables -t nat -A POSTROUTING -o ens5 -j MASQUERADE
 sudo /sbin/iptables -F FORWARD
 sudo service iptables save
 EOF

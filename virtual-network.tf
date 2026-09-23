18c460bfd4f3f2573cea1b02a7233c63f89364e8
Benjamin Tucker <benjamin.tucker@kontron.com>
2026-09-23 16:13:57 +0100

    Switch NAT configuration to use external NAT instance and disable private NAT routes

--- virtual-network.tf
+++ virtual-network.tf
@@ -29,11 +29,7 @@ module "vpc" {
   public_subnets       = [local.public_cidr]
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  // Expensive, so we will use a NAT Instance
-  enable_nat_gateway         = false
-  single_nat_gateway         = false
-  manage_default_route_table = false
+  external_nat_ip_ids  = [aws_instance.nat_ec2_instance.primary_network_interface_id]
 
   tags = local.tags
 }
